3a532e0656cce1ad58cc69ec3cc3b3324535be79
qunlingwang <qunlingwang@gmail.com>
2025-01-27 15:01:10 -0800

    Fixed a bug

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 
 import './style.css';
-document.addEventListener('DOMContentLoaded', () => {
+if (document.readyState === 'loading') {
+  document.addEventListener('DOMContentLoaded', () => {
     const textInput = document.getElementById('textInput');
     const readAloudButton = document.getElementById('readAloud');
-  
+
     readAloudButton.addEventListener('click', () => {
       const text = textInput.value;
-      
+
       fetch('https://my-text-to-speech.onrender.com/synthesize', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ text }),
       })
-      .then(response => response.blob())
+        .then(response => response.blob())
         .then((data) => {
           const audio = new Audio(URL.createObjectURL(data));
           audio.play();
@@ -20,4 +21,4 @@ document.addEventListener('DOMContentLoaded', () => {
         .catch((error) => console.error('Error:', error));
     });
   });
-  
+}
